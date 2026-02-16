3d17ea25e9862fc052a471f13ad57de20db88f14
Joana <joanalluna.f@gmail.com>
2026-02-16 19:50:14 -0100

    commit new menu structure

---
+++ the_space_inside_my_legs/script.js
@@ -0,0 +1,12 @@
+
+const videos = document.querySelectorAll('.video');
+
+videos.forEach(video => {
+  video.addEventListener('click', () => {
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  });
+});
